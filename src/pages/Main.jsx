9eacf8894683dc6a2262694a2a94976f700ac176
elfinana <elfinana@naver.com>
2023-05-06 09:24:40 +0900

    Feat: 메인페이지 css 작업

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
-const Header = styled.header`
+const InfoBox = styled.div`
   width: 100%;
   background: #fbe8e7;
   height: 200px;
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const Header = styled.div`
+  background-color: #fbe8e7;
 
   div {
-    position: fixed;
-    right: 0;
-    margin-right: 10px;
+    float: right;
   }
 `;
 
@@ -32,11 +34,12 @@ const WriteBox = styled.div`
 `;
 
 const ListWrap = styled.div`
-  background-color: red;
+  /* background-color: red; */
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
   margin-top: 30px;
   width: 70%;
   height: 100%;
-  display: flex;
   justify-content: center;
   align-items: center;
   margin-left: 50px;
@@ -44,18 +47,23 @@ const ListWrap = styled.div`
 
 const ImageBox = styled.div`
   flex-direction: column;
+  margin-right: 30px;
+  margin-bottom: 30px;
 `;
 
 const Image = styled.image`
-  width: 30px;
-  height: 30px;
+  display: block;
+  width: 270px;
+  height: 250px;
   border: 1px solid red;
 `;
 
 const VideoBox = styled.video`
-  float: right;
   height: 100%;
   width: 20%;
+  position: absolute;
+  right: 0;
+  top: 230px;
   background-color: red;
   margin-right: 50px;
 `;
@@ -64,10 +72,12 @@ function Main() {
   return (
     <>
       <Header>
-        사이트 이름 및 소개
+        <image>로고</image>
         <div>로그인</div>
         <div>회원가입</div>
       </Header>
+
+      <InfoBox>사이트 이름 및 소개</InfoBox>
 
       <WriteBox>
         작성하기
@@ -75,6 +85,22 @@ function Main() {
       </WriteBox>
 
       <ListWrap>
+        <ImageBox>
+          <Image>조선아</Image>
+          <div>타이틀</div>
+        </ImageBox>
+        <ImageBox>
+          <Image>조선아</Image>
+          <div>타이틀</div>
+        </ImageBox>
+        <ImageBox>
+          <Image>조선아</Image>
+          <div>타이틀</div>
+        </ImageBox>
+        <ImageBox>
+          <Image>조선아</Image>
+          <div>타이틀</div>
+        </ImageBox>
         <ImageBox>
           <Image>조선아</Image>
           <div>타이틀</div>
